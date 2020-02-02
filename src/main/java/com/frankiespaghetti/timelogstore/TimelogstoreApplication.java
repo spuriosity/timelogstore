382cbf11e89470bfdf893282a958fb42a6002347
FrankieSpaghetti <jackinberlin+git@gmail.com>
2020-02-02 11:23:38 +0000

    initial commit

---
+++ src/main/java/com/frankiespaghetti/timelogstore/TimelogstoreApplication.java
@@ -0,0 +1,13 @@
+package com.frankiespaghetti.timelogstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelogstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelogstoreApplication.class, args);
+	}
+
+}
